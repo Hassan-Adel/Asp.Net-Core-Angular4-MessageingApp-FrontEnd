22334e195d5656a061c108c364bc0ae3465ca6c8
Hassan Adel <hassan.adel@RMGS.CO>
2017-07-26 12:38:19 +0200

    Displaying a dummy messages array

--- src/app/messages-component.ts
+++ src/app/messages-component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component'
+    template: 'this is the messages component : <div *ngFor="let message of messages">{{message.text}} - By : {{message.owner}}<div>'
 })
 export class MessagesComponent{
-
+    messages= [{text: 'text', owner: 'Hassan'}, {text: 'my message', owner: 'User'}];
 }
