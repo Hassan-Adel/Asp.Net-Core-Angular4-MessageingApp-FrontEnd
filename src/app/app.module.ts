1f1c1a5763efaa9c0af38e1e1520b0fe0cb54c10
Hassan Adel <hassan.adel@RMGS.CO>
2017-07-26 16:19:56 +0200

    This Commit is ESSENTIAL for the angular material library to work. Including : updating typescript to "~2.2.2" and rxjs to "5.3.0" and adding the required sources to system.comfig.js to avoid (404 no found) errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
 import { AppComponent }  from './app.component';
 import { MessagesComponent } from './messages-component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, MaterialModule, BrowserAnimationsModule ],
   declarations: [ AppComponent, MessagesComponent ],
   bootstrap:    [ AppComponent ]
 })
