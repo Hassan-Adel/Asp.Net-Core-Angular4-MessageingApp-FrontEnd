002ae0ce79dc8bfdbc8f1cac082601138f538431
Hassan Adel <hassan.adel@RMGS.CO>
2017-07-26 16:20:33 +0200

    Adding <md-card> demo

--- src/app/messages-component.ts
+++ src/app/messages-component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'messages',
-    template: 'this is the messages component : <div *ngFor="let message of messages">{{message.text}} - By : {{message.owner}}<div>'
+    template: `
+    <div *ngFor="let message of messages">
+    <md-card>
+    {{message.text}} - By : {{message.owner}}
+    </md-card>
+    <div>
+    `
 })
 export class MessagesComponent{
     messages= [{text: 'text', owner: 'Hassan'}, {text: 'my message', owner: 'User'}];
