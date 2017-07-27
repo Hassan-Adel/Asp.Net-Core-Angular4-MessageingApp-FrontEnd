99b1a28f3e034be4fce1c388fd3ab53e55dc8d6e
Hassan Adel <hassan.adel@RMGS.CO>
2017-07-27 03:22:10 +0200

    Updating messages component to get data from the service

--- src/app/messages-component.ts
+++ src/app/messages-component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebService } from './web.service';
 
 @Component({
     selector: 'messages',
@@ -12,5 +13,19 @@ import { Component } from '@angular/core';
     `
 })
 export class MessagesComponent{
-    messages= [{text: 'text', owner: 'Hassan'}, {text: 'my message', owner: 'User'}];
+
+    constructor(private webService : WebService) {}
+
+/*
+If we try this without "async/await:, 
+it would not work because it would just try to console log the promise 
+not the response since inside web service we are returning a promise. 
+And so what we need to do is wait for the response to come in through the promise.
+ */
+    async ngOnInit(){
+        var response = await this.webService.getMessages();
+        this.messages = response.json();
+    }
+
+    messages: any[];
 }
