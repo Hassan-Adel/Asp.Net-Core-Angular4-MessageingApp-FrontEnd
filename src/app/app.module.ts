3d5fa169925f77142d3f3ba2491432149c9342e2
Hassan Adel <hassan.adel@RMGS.CO>
2017-07-27 03:21:46 +0200

    Adding HttpModule and WebService Component to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule }      from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MaterialModule } from '@angular/material';
 import { AppComponent }  from './app.component';
 import { MessagesComponent } from './messages-component';
+import { WebService } from './web.service';
 
 @NgModule({
-  imports:      [ BrowserModule, MaterialModule, BrowserAnimationsModule ],
+  imports:      [ BrowserModule, HttpModule, MaterialModule, BrowserAnimationsModule ],
   declarations: [ AppComponent, MessagesComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [ WebService ]
 })
 export class AppModule { }
