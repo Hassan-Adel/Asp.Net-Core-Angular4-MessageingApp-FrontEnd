03c6e0a5b4a1cfd7d7d3312ce75bf0a3e4cfea2a
Hassan Adel <hassan.adel@RMGS.CO>
2017-07-29 00:38:12 +0200

    Adding Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import{MessagesComponent} from './messages-component';
-import { NewMessageComponent } from "./new-message.component";
 import { NavComponent } from './nav.component';
 
 @Component({
   selector: 'my-app',
   template: `
   <nav></nav>
-  <new-message></new-message>
-  <messages></messages>
+  <router-outlet></router-outlet>
   `,
 })
 export class AppComponent  { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HomeComponent } from './home.component';
 import { NavComponent } from './nav.component';
 import { NgModule }      from '@angular/core';
+import {RouterModule} from '@angular/router';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -10,9 +12,14 @@ import { WebService } from './web.service';
 import { NewMessageComponent } from "./new-message.component";
 import { FormsModule } from "@angular/forms";
 
+var routes=[
+  { path: '', component: HomeComponent },
+  { path: 'messages', component: MessagesComponent }
+];
+
 @NgModule({
-  imports:      [ BrowserModule, HttpModule, MaterialModule, BrowserAnimationsModule, FormsModule ],
-  declarations: [ AppComponent, MessagesComponent, NewMessageComponent, NavComponent ],
+  imports:      [ BrowserModule, HttpModule, MaterialModule, BrowserAnimationsModule, FormsModule, RouterModule.forRoot(routes) ],
+  declarations: [ AppComponent, MessagesComponent, NewMessageComponent, NavComponent, HomeComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ WebService ]
 })
