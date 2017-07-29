d23d92724b723650d84e1df553e3dfb344ed0b19
Hassan Adel <hassan.adel@RMGS.CO>
2017-07-29 03:26:41 +0200

    Getting Username from the route params

--- src/app/messages-component.ts
+++ src/app/messages-component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { WebService } from './web.service';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: 'messages',
     template: `
     <div *ngFor="let message of webService.messages">
         <md-card class="mt-8">
-            <md-card-title>{{message.owner}}</md-card-title> 
+            <md-card-title [routerLink]="['/messages', message.owner]" class="cursor">{{message.owner}}</md-card-title> 
             <md-card-content>{{message.text}}</md-card-content>
         </md-card>
     <div>
@@ -14,5 +15,8 @@ import { WebService } from './web.service';
 })
 export class MessagesComponent {
 
-    constructor(private webService: WebService) { }
+    constructor(private webService: WebService, private route: ActivatedRoute) { }
+    ngOnInit(){
+            console.log(this.route.snapshot.params.name);
+        }
 }
