9aca2e54507cc1cd8de58a753bda88c1c7b003d8
Hassan Adel <hassan.adel@RMGS.CO>
2017-07-29 21:17:45 +0200

    Adding a register Componenrt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RegisterComponent } from './register.component';
 import { HomeComponent } from './home.component';
 import { NavComponent } from './nav.component';
 import { NgModule }      from '@angular/core';
@@ -15,12 +16,13 @@ import { FormsModule } from "@angular/forms";
 var routes=[
   { path: '', component: HomeComponent },
   { path: 'messages', component: MessagesComponent },
-  { path: 'messages/:name', component: MessagesComponent }
+  { path: 'messages/:name', component: MessagesComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
   imports:      [ BrowserModule, HttpModule, MaterialModule, BrowserAnimationsModule, FormsModule, RouterModule.forRoot(routes) ],
-  declarations: [ AppComponent, MessagesComponent, NewMessageComponent, NavComponent, HomeComponent ],
+  declarations: [ AppComponent, MessagesComponent, NewMessageComponent, NavComponent, HomeComponent, RegisterComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ WebService ]
 })
---
+++ src/app/register.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { NavComponent } from './nav.component';
+
+@Component({
+    moduleId: module.id,
+  selector: 'register',
+  templateUrl:'register.component.html'
+})
+export class RegisterComponent  { }
